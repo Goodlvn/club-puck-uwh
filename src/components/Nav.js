ef8e3bdf73a70f0710571e2d45a0479ff7963c27
JD Martinez <focus4ursoul@gmail.com>
2021-03-08 20:30:21 -0800

    restructure html for navigation styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 
 export default function Nav() {
+
+    const router = useRouter();
 
     const [hoverState, setHoverState] = useState({
         news: false,
@@ -11,45 +14,67 @@ export default function Nav() {
         contact: false
     })
 
+    console.log(router.pathname);
+
+    const toInvertOut = () => {
+        setHoverState({ ...hoverState, news: false })
+    }
+
+    const toInvertIn = () => {
+        setHoverState({ ...hoverState, news: true })
+    }
+
     return (
         <div className="navBtnContainer">
-            <Link href="/news">
-                <div className="navBtn news"
-                    onMouseEnter={() => { setHoverState({ ...hoverState, news: true }) }}
-                    onMouseLeave={() => { setHoverState({ ...hoverState, news: false }) }}
-                >
-                    <img
-                        src={hoverState.news ? "./images/newsInv.svg" : "./images/news.svg"} className={"newsimg"} />
-                    <p id="news" className="btnLabel">News</p>
-                </div>
-            </Link>
-            <Link href="/learn">
-                <div className="navBtn learn"
-                    onMouseEnter={() => { setHoverState({ ...hoverState, learn: true }) }}
-                    onMouseLeave={() => { setHoverState({ ...hoverState, learn: false }) }}
-                >
-                    <img src={hoverState.learn ? "./images/learnInv.svg" : "./images/learn.svg"} className="learnimg" />
-                    <p id="learn" className="btnLabel">Learn More</p>
-                </div>
-            </Link>
-            <Link href="/youth">
-                <div className="navBtn youth"
-                    onMouseEnter={() => { setHoverState({ ...hoverState, youth: true }) }}
-                    onMouseLeave={() => { setHoverState({ ...hoverState, youth: false }) }}
-                >
-                    <img src={hoverState.youth ? "./images/youthInv.svg" : "./images/youth.svg"} className="youthimg" />
-                    <p id="youth" className="btnLabel">Youth UWH</p>
-                </div>
-            </Link>
-            <Link href="/contact">
-                <div className="navBtn contact"
-                    onMouseEnter={() => { setHoverState({ ...hoverState, contact: true }) }}
-                    onMouseLeave={() => { setHoverState({ ...hoverState, contact: false }) }}
-                >
-                    <img src={hoverState.contact ? "./images/sendInv.svg" : "./images/send.svg"} className="sendimg" />
-                    <p id="contact" className="btnLabel">Contact Us</p>
-                </div>
-            </Link>
+
+            <div className="btnWrapper">
+                <p id="news" className="btnLabel" style={hoverState.news ? { opacity: "1" } : { opacity: "0" }}>News</p>
+                <Link href="/news">
+                    <div className="navBtn news"
+                        onMouseEnter={() => toInvertIn()}
+                        onMouseLeave={() => toInvertOut()}
+                    >
+                        <img
+                            src={(hoverState.news) ? "./images/newsInv.svg" : "./images/news.svg"} className={"newsimg"} />
+                    </div>
+                </Link>
+            </div>
+
+            <div className="btnWrapper">
+                <p id="learn" className="btnLabel" style={hoverState.learn ? { opacity: "1" } : { opacity: "0" }}>Learn More</p>
+                <Link href="/learn">
+                    <div className="navBtn learn"
+                        onMouseEnter={() => { setHoverState({ ...hoverState, learn: true }) }}
+                        onMouseLeave={() => { setHoverState({ ...hoverState, learn: false }) }}
+                    >
+                        <img src={hoverState.learn ? "./images/learnInv.svg" : "./images/learn.svg"} className="learnimg" />
+                    </div>
+                </Link>
+            </div>
+
+            <div className="btnWrapper">
+                <p id="youth" className="btnLabel" style={hoverState.youth ? { opacity: "1" } : { opacity: "0" }}>Youth UWH</p>
+                <Link href="/youth">
+                    <div className="navBtn youth"
+                        onMouseEnter={() => { setHoverState({ ...hoverState, youth: true }) }}
+                        onMouseLeave={() => { setHoverState({ ...hoverState, youth: false }) }}
+                    >
+                        <img src={hoverState.youth ? "./images/youthInv.svg" : "./images/youth.svg"} className="youthimg" />
+                    </div>
+                </Link>
+            </div>
+
+            <div className="btnWrapper">
+                <p id="contact" className="btnLabel" style={hoverState.contact ? { opacity: "1" } : { opacity: "0" }}>Contact Us</p>
+                <Link href="/contact">
+                    <div className="navBtn contact"
+                        onMouseEnter={() => { setHoverState({ ...hoverState, contact: true }) }}
+                        onMouseLeave={() => { setHoverState({ ...hoverState, contact: false }) }}
+                    >
+                        <img src={hoverState.contact ? "./images/sendInv.svg" : "./images/send.svg"} className="sendimg" />
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
